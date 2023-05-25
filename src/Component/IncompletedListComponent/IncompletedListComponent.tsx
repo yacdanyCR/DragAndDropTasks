1749d3f4c08473d1b22728ada0659286674e3458
Yacdany Arguedas Mora <jack20-21@hotmail.com>
2023-05-25 12:59:33 -0600

    Drag

--- src/Component/IncompletedListComponent/IncompletedListComponent.tsx
+++ src/Component/IncompletedListComponent/IncompletedListComponent.tsx
@@ -2,22 +2,48 @@ import { useContext } from 'react'
 import './style.css'
 import { TaskContext } from '../../Context/TaskContext'
 import { listTask } from '../../Interface/TaskInterface'
+import { DragDropContext, Draggable, DropResult, Droppable } from 'react-beautiful-dnd'
+
 
 const IncompletedListComponent = () => {
 	const objtTask = useContext(TaskContext)
 	const incompletedTasks: listTask[] = objtTask.listTask.filter((el) => !el.completed)
 
+	const handleDragEnd = (result: DropResult) => {
+		if (result.destination) {
+			const destination = result.destination.index
+			const previousObj = incompletedTasks[destination]
+			const currentObj = incompletedTasks.findIndex((el) => el.id === result.draggableId)
+
+			incompletedTasks.splice(destination, 1, incompletedTasks[currentObj])
+			incompletedTasks.splice(currentObj, 1, previousObj)
+		}
+	}
+
 	return (
-		<div className='incompleted__List'>
-			<div className="incompleted__list_title">
-				<h2>Incompleted Tasks</h2>
+		<DragDropContext onDragEnd={(result) => handleDragEnd(result)}>
+			<div className='incompleted__List'>
+				<div className="incompleted__list_title">
+					<h2>Incompleted Tasks</h2>
+				</div>
+				<Droppable droppableId='tasks'>
+					{(droppableProvided) =>
+						<ul {...droppableProvided.droppableProps} ref={droppableProvided.innerRef} className='incompleted__ul_List'>
+							{incompletedTasks.map((el, index) => (
+								<Draggable key={el.id} draggableId={el.id} index={index}>
+									{(draggableProvided) =>
+										<li {...draggableProvided.draggableProps}
+											ref={draggableProvided.innerRef}
+											{...draggableProvided.dragHandleProps}
+										>{el.task}</li>
+									}
+								</Draggable>
+							))}
+							{droppableProvided.placeholder}
+						</ul>}
+				</Droppable>
 			</div>
-			<ul className='incompleted__ul_List'>
-				{incompletedTasks.map((el) => (
-					<li>{el.task}</li>
-				))}
-			</ul>
-		</div>
+		</DragDropContext>
 	)
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,9 @@ import { TaskProvider } from './Context/TaskContext.tsx'
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <TaskProvider>
-      <App />
-    </TaskProvider>
-  </React.StrictMode>,
+  //<React.StrictMode>
+  <TaskProvider>
+    <App />
+  </TaskProvider>
+  //</React.StrictMode>,
 )
